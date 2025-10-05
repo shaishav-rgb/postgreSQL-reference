5af12c1c84ec23b01a2475714b0368682af02069
shaishav-rgb <shaishavpaudel@gmail.com>
2025-10-05 14:36:31 +0545

    feat: add more transaction isolation examples

--- postgreSQL-reference/Scripts/transaction-isolation-level.sql
+++ postgreSQL-reference/Scripts/transaction-isolation-level.sql
@@ -1,4 +1,4 @@
---REPEATABLE READ
+--REPEATABLE READ(Case 1)
 CREATE TABLE dummytags(id int, tag text);
 --INSERT INTO tags
 	--SESSION 1
@@ -14,6 +14,20 @@ select * from dummytags where tag='DA'; -- got data
 update  dummytags set tag=lower(tag) || 'g' where tag='DA';
 --ERROR:  could not serialize access due to concurrent update
 
+--REPEATABLE READ(Case 1), this is the same behavior even in serializable isolation level because data is
+--readonly IS SESSION 2, incase of data in another record is changed(say id 3) then serializable mode fails
+--AND REPEATABLE READ passes. Seralizable mode fails even when doing 'select * from dummytags' after 
+--SESSION 1 has commited
+	insert into dummytags(id,tag) values(7,'jpt');
+	--session 1
+	set transaction isolation level repeatable read;
+	delete from dummytags where id=7;
+COMMIT;
+	--session 2
+	set transaction isolation level repeatable read;
+count(*) from dummytags; --(NO info OF id 7 being deleted)
+COMMIT;
+--after commit we get that the id 7 is deleted in another concurrent transaction;
 
 --serializable(In serializable transaction isolation level, it fails even when the data being updated 
 --are different but it passes IN REPEATABLE READ level)
