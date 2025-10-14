2f821d540874f32b9c360d2dd41104292bf68ec8
shaishav-rgb <shaishavpaudel@gmail.com>
2025-10-14 08:09:05 +0545

    feat: add lateral query

--- postgreSQL-reference/Scripts/part-three.sql
+++ postgreSQL-reference/Scripts/part-three.sql
@@ -50,6 +50,56 @@ from artist,
 lateral get_all_albums(artist_id)
 where artist.name = 'Red Hot Chili Peppers';
 
+select genre.name, count(*) as count
+from
+ genre
+left join track using(genre_id)
+group by genre.name
+order by count desc;
+
+--Example of lateral join
+-- name: genre-top-n
+-- Get the N top tracks by genre
+select genre.name as genre,
+case when length(ss.name) > 15
+then substring(ss.name from 1 for 15) || '⋯'
+else ss.name
+end as track,
+artist.name as artist
+from genre left join lateral
+/*
+* the lateral left join implements a nested loop over
+* the genres and allows to fetch our Top-N tracks per
+* genre, applying the order by desc limit n clause.
+*
+* here we choose to weight the tracks by how many
+* times they appear in a playlist, so we join against
+* the playlisttrack table and count appearances.
+*/
+(
+select track.name, track.album_id, count(playlist_id)
+from
+ track
+left join playlist_track using (track_id)
+where track.genre_id = genre.genre_id
+group by track.track_id
+order by count desc
+limit 3
+)
+/*
+* the join happens in the subquery's where clause, so
+* we don't need to add another one at the outer join
+* level, hence the "on true" spelling.
+*/
+ss(name, album_id, count) on true
+join album using(album_id)
+join artist using(artist_id)
+order by genre.name, ss.count desc;
+
+
+
 select * from album;
 select * from artist;
 select * from track;
+select * from genre;
+select * from playlist_track;
