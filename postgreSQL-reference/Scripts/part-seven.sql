2bf4e52c70fa76eea99650647ac3113ec3b7c4d3
shaishav-rgb <shaishavpaudel@gmail.com>
2025-10-25 09:03:14 +0545

    feat: add notify trigger

--- postgreSQL-reference/Scripts/part-seven.sql
+++ postgreSQL-reference/Scripts/part-seven.sql
@@ -73,5 +73,47 @@ values (7, 'rt'),
 select day, rts, de_rts, favs, de_favs
 from twcache.daily_counters;
 
+begin;
+create or replace function twcache.tg_notify_counters ()
+returns trigger
+language plpgsql
+as $$
+declare
+channel text := TG_ARGV[0];
+begin
+PERFORM (
+with payload(messageid, rts, favs) as
+(
+select NEW.messageid,
+coalesce(
+case NEW.action
+when 'rt'
+ then 1
+when 'de-rt' then -1
+end,
+0
+) as rts,
+coalesce(
+case NEW.action
+when 'fav'
+ then 1
+when 'de-fav' then -1
+end,
+0
+) as favs
+)
+select pg_notify(channel, row_to_json(payload)::text)
+from payload
+);
+RETURN NULL;
+end;
+$$;
+CREATE TRIGGER notify_counters
+AFTER INSERT
+ON tweet.activity
+FOR EACH ROW
+EXECUTE PROCEDURE twcache.tg_notify_counters('tweet.activity');
+commit;
+
 
 rollback;
