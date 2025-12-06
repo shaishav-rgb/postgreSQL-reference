05e27b541d414388bf813f0684ff65980e517d02
shaishav-rgb <shaishavpaudel@gmail.com>
2025-12-06 16:22:05 +0545

    feat: add repeatable read concurrent update error

--- postgreSQL-reference/Scripts/postgres-locks.sql
+++ postgreSQL-reference/Scripts/postgres-locks.sql
@@ -47,7 +47,7 @@ commit;
 
 --LOCKS
 --1.) For update locks
---	blocks insert in foreign key remote table
+--	blocks insert  in referencing table due to foreign key
 -- does not allow any update/delete in the locked table
 -- exclusive lock
 	begin;
@@ -55,7 +55,7 @@ commit;
 	commit;
 
 --2.) For no key update locks
-	--	does not block insert in foreign key remote table
+	--	does not block insert in referencing table
 	-- does not allow any update/delete in the locked table
 	-- exclusive lock
 	begin;
@@ -63,7 +63,7 @@ commit;
 	commit;
 
 --3.) For share locks
-	--	does not block insert in foreign key remote table
+	--	does not block insert in referencing table
 	-- does not allow any update/delete in the locked table but locks can be shared
 	begin;
 	select * from dummytags for share;
@@ -74,7 +74,7 @@ commit;
 	end;
 	
 --4.) for key share
-	--	does not block insert in foreign key remote table
+	--	does not block insert in referencing table
 	-- allow non-key columns update in the locked rows and is a shared lock,but disallow row deletion even when non of the columns are unique in the locked rows
 	-- does not allow deletion of locked row
 	-- does not allow update of unique/primary column in the locked table
@@ -90,10 +90,43 @@ insert into dummytags_non_unique values(1,'a'),(2,'b'),(3,'c'),(5,'e'),(6,'f'),(
 select * from dummytags_non_unique for key share;
 	end;
 	
---Allows updating non-unique column in key share row lock but disallaow deletion of the row in the locked rows
+--Allows updating non-unique column in key share row lock but disallaow deletion of the row in the locked rows even when non of the column are unique
 		begin;
 	delete from dummytags_non_unique where id=10;
 	commit;
+
+
+--transaction levels Reference 
+
+--#REPEATABLE READ#
+--1.) Normal corcurrent update
+begin transaction isolation level repeatable read;
+begin;
+	select * from dummytags;
+--in this mean time if some transaction update the same below row then repatable read throws "SQL Error [40001]: ERROR: could not serialize access due to concurrent 
+--update", if reading or inserting a new row in this mean time, then repeatable read does not throw error
+	update dummytags set tag='rhino' where id=1;
+commit;
+
+
+--2.) using row level locks
+begin transaction isolation level repeatable read;
+begin;
+	select * from dummytags;
+--in this mean time, if some transaction update id=3 then below queries will eventually throw 40001 error after pausing for some time, when that "some" transaction 
+--commit then the pause aborts and below queries throw error
+select * from dummytags where id=3 for update;
+select * from dummytags where id=3 for no key update;
+
+--in this mean time, if some transaction update id=3 then below queries will immediately throw 40001 error
+update dummytags set tag='rhino' where id=1;
+commit;
+
+
+#SERIALIZABLE#
+
+--1.)
+	
 
 	
 
