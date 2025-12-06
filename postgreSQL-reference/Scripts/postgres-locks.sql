1a59bd41ad46fbb75b4c7e160f19bb050e372398
shaishav-rgb <shaishavpaudel@gmail.com>
2025-12-06 14:08:11 +0545

    feat: add non-unique columns in for key share row lock

--- postgreSQL-reference/Scripts/postgres-locks.sql
+++ postgreSQL-reference/Scripts/postgres-locks.sql
@@ -1,6 +1,11 @@
 CREATE TABLE dummytags(id int not null unique, tag text);
 
-alter table dummytags add column metatags text;
+alter table dummytags alter column unique_num set not null;
+
+alter table dummytags add  unique(unique_num);
+
+update dummytags d  set unique_num=v.num from (values (1,101),(2,102),(3,103),(5,105),(6,106),(7,107),(8,108),(10,110)) as v(id,num) where d.id=v.id;
+
 
 create table dummytags_reference(id int not null unique, description text,tagId integer references dummytags(id) on delete cascade);
 
@@ -70,12 +75,26 @@ commit;
 	
 --4.) for key share
 	--	does not block insert in foreign key remote table
-	-- allow non-key columns update and is a shared lock
+	-- allow non-key columns update in the locked rows and is a shared lock,but disallow row deletion even when non of the columns are unique in the locked rows
 	-- does not allow deletion of locked row
-	-- does not allow update of foreign-keyed column in the locked table
+	-- does not allow update of unique/primary column in the locked table
 	begin;
 select * from dummytags for key share;
 	end;
+
+--Testing for key share with non-keyed colum
+	CREATE TABLE dummytags_non_unique(id int not null, tag text,metatags text);
+insert into dummytags_non_unique values(1,'a'),(2,'b'),(3,'c'),(5,'e'),(6,'f'),(7,'g'),(8,'h');
+	
+	begin;
+select * from dummytags_non_unique for key share;
+	end;
+	
+--Allows updating non-unique column in key share row lock but disallaow deletion of the row in the locked rows
+		begin;
+	delete from dummytags_non_unique where id=10;
+	commit;
+
 	
 
 
